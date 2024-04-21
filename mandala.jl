c07a84b91dd02aa7f6a1e860add65635845c95ea
Ahmed Riza <dr.riza@gmail.com>
2024-04-21 01:27:28 +0100

    Learning Julia

--- mandala.jl
+++ mandala.jl
@@ -4,7 +4,7 @@ function p(a, b, c, theta)
   return a .* cos.(b .* theta) .+ c
 end
 
-theta = range(0, 31.4, length=1000)
+theta = range(0, 10π, length=1000)
 
 # plot(theta, p(theta), q(theta), label="pq(θ)", lw=1, legend=:topleft)
 
@@ -12,7 +12,7 @@ theta = range(0, 31.4, length=1000)
 # rs = r.(theta)
 # plot(theta, rs, proj=:polar, label="r(θ)", lw=1, legend=:topleft)
 
-a = 1.5
+a = 0.5
 b = 6.4
 c = 2
 plot(theta, p.(a, b, c, theta), proj=:polar, label="r(θ)", lw=1, legend=:topleft)
